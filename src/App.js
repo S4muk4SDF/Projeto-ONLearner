48ef1fcf0361c54f59886aac35644ba8eba5496c
S4muk4SDF <sdf.dias.faria@gmail.com>
2020-11-02 17:41:57 -0300

    Protótipo home feita (teste para a hospedagem)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home'
+
+function App () {
+    return (
+      <div>
+        <Route exact path="/" component={Home}/>
+{/*         
+        <Route path="/matematica" component={Matematica}/>
+        <Route path="/portugues" component={Portugues}/>
+        <Route path="/quimica" component={Quimica}/>
+        <Route path="/biologia" component={Biologia}/>
+        <Route path="/historia" component={Historia}/>
+        <Route path="/geografia" component={Geografia}/> */}
+      </div>
+    );
 }
 
 export default App;
---
+++ src/components/Home/Navbar/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './style.css'
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+
+                <div className="Navbar">
+                        <h1>ONLearner</h1>
+
+                        <div className="login">
+                            <a>Registrar-se</a>
+                            <a>Entrar</a>
+                        </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default NavBar;
